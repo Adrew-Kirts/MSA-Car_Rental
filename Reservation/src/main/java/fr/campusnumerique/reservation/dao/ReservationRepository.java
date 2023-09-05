ebdf6296a9d0fffc706621e83cb876e4ffeb01bf
Ezra Strikwerda <ezra@strikwerda.fr>
2023-09-05 15:02:36 +0200

    Added dao

---
+++ Reservation/src/main/java/fr/campusnumerique/reservation/dao/ReservationRepository.java
@@ -0,0 +1,7 @@
+package fr.campusnumerique.reservation.dao;
+
+import fr.campusnumerique.reservation.model.Reservation;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
+}
