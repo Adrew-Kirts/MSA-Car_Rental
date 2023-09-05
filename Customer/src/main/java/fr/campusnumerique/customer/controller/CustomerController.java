ef72be828a55325516a42ef0c47627d3d05e5506
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-09-05 10:51:34 +0200

    customer CRUD

--- Customer/src/main/java/fr/campusnumerique/customer/controller/CustomerController.java
+++ Customer/src/main/java/fr/campusnumerique/customer/controller/CustomerController.java
@@ -4,12 +4,8 @@ package fr.campusnumerique.customer.controller;
 import fr.campusnumerique.customer.dao.CustomerRepository;
 import fr.campusnumerique.customer.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -29,19 +25,11 @@ public class CustomerController {
     public Optional<Customer> getCustomersById(@PathVariable int id){ return customerRepository.findById(id); }
 
     @PostMapping
-    public ResponseEntity<Customer> addCustomers(@RequestBody Customer customer){
+    public Optional<Customer> addCustomers(@RequestBody Customer customer){
         //License Validator
         //Age Validator(18)
         Customer customerAdded = customerRepository.save(customer);
-        if (Objects.isNull(customerAdded)){
-            return ResponseEntity.noContent().build();
-        }
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(customerAdded.getId())
-                .toUri();
-        return ResponseEntity.created(location).build();
+        return Optional.of(customerAdded);
     }
 
     @PutMapping
