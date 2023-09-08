829041cc5b3e181227c54e5ac1e3443db0ce0bbf
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-09-08 14:22:48 +0200

    setmaintenanceresa UPD

--- Reservation/src/main/java/fr/campusnumerique/reservation/dao/ReservationRepository.java
+++ Reservation/src/main/java/fr/campusnumerique/reservation/dao/ReservationRepository.java
@@ -13,10 +13,10 @@ public interface ReservationRepository extends JpaRepository<Reservation, Intege
     @Query(value="SELECT * FROM reservation r WHERE r.vehicle_id = :vehicleId AND r.rental_start > LOCALTIME", nativeQuery = true)
     List<Reservation> findReservationPlanning(@Param("vehicleId") int VehicleId);
 
-    @Query(value="SELECT * FROM reservation r WHERE r.vehicle_id = :vehicleID AND r.rental_start < :rentalStart < r.rental_end AND r.rental_start < :rentalStart < r.rental_end", nativeQuery = true)
+    @Query(value="SELECT * FROM reservation r WHERE r.vehicle_id = :vehicleId AND r.rental_start < :rentalStart < r.rental_end AND r.rental_start < :rentalStart < r.rental_end", nativeQuery = true)
     boolean isAvailable(@Param("vehicleId") int vehicleId, @Param("rentalStart")LocalDate rentalStart, @Param("rentalEnd")LocalDate rentalEnd);
 
-    @Query(value="SELECT * FROM reservation r WHERE r.customer_id = :customerID AND r.rental_start < :rentalStart < r.rental_end AND r.rental_start < :rentalStart < r.rental_end", nativeQuery = true)
+    @Query(value="SELECT * FROM reservation r WHERE r.customer_id = :customerId AND r.rental_start < :rentalStart < r.rental_end AND r.rental_start < :rentalStart < r.rental_end", nativeQuery = true)
     boolean isDisengage(@Param("customerId") int customerId, @Param("rentalStart")LocalDate rentalStart, @Param("rentalEnd")LocalDate rentalEnd);
 
 }
