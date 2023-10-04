b3b1708d7cb817ec33c040a111ef3b92d3f8fe81
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-10-04 11:01:58 +0200

    add Vehicle, VehicleList and VehicleReservation

---
+++ Vrooom/src/views/VehicleReservation.vue
@@ -0,0 +1,31 @@
+<script>
+import VehiclesService from "@/services/VehiclesService";
+
+export default {
+  name: "VehicleReservation",
+  data(){
+    return{
+      Vehicle:{}
+    }
+  },
+  methods:{
+    getVehicle(id){
+      VehiclesService.getVehicle(id)
+          .then(response=>this.Vehicle=response.data)
+    }
+  },
+  created(){
+    this.getVehicle(this.$route.params.id)
+  }
+}
+</script>
+
+<template>
+<h3>{{ Vehicle }}</h3>
+
+
+</template>
+
+<style scoped>
+
+</style>
