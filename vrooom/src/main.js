a78b4af4e1929c1006d385d6e22ba2c496d56402
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-10-04 09:43:45 +0200

    update ant

--- vrooom/src/main.js
+++ vrooom/src/main.js
@@ -7,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 // import de Ant
 import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
+import "ant-design-vue/dist/reset.css";
 
 const app = createApp(App)
 
