53050e4e28d819c08981c3b7b3c835dee2fe31ad
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-10-04 09:23:33 +0200

    add Ant

--- vrooom/src/main.js
+++ vrooom/src/main.js
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// import de Ant
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// register Ant globally
+app.use(Antd).mount("#app");
 
 app.mount('#app')
