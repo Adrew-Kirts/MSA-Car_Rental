52a3bd92eff492a746589189f7465b8114ee0f60
Ezra Strikwerda <ezra@strikwerda.fr>
2023-10-04 11:07:12 +0200

    Updated .env and VehicleService

--- Vrooom/src/services/VehiclesService.js
+++ Vrooom/src/services/VehiclesService.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const VEHICLE_API = 'http://192.168.1.239:8086/vehicles/';
+const VEHICLE_API_URL = import.meta.env.VITE_VEHICLE_BASE_URL
 
 class VehiclesService{
     getVehicles(){
-        return axios.get(VEHICLE_API);
+        return axios.get(VEHICLE_API_URL);
     }
     getVehicle(id){
-        return axios.get(VEHICLE_API+id);
+        return axios.get(VEHICLE_API_URL+id);
     }
 }
 
